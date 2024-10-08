1846bc6c5841a83a5ba883b6651eba0d7f0e8e6b
Yocchi3191 <yoshioyocchi314@gmail.com>
2024-10-09 06:44:34 +0900

    基本フォルダを追加。プレイヤー移動を実装中。

---
+++ Assets/Scripts/Actors/Actor.cs
@@ -0,0 +1,17 @@
+using System.Numerics;
+
+public abstract class Actor
+{
+	Vector2 currentPos;
+	int currentMapID;
+
+	protected bool CanMove(Vector2 pos)
+	{
+		bool canMove;
+		Map map = Map(currentMapID);
+		if()
+		return canMove;
+	}
+
+
+}
