73f8ecd15dda547e32e35981d4ac0d6bbfcd43eb
Yocchi3191 <yoshioyocchi314@gmail.com>
2024-11-28 08:19:55 +0000

    2024年11月28日 17:19

--- Assets/Scripts/InputHandler/MoveInputHandler.cs
+++ Assets/Scripts/InputHandler/MoveInputHandler.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,6 +11,7 @@ namespace BokenshaChan
 		private Vector2 _currentMoveInput;
 		private bool _isMoving;
 		[SerializeField] private float _moveCheckInterval = 0.1f;
+
 		private void Awake()
 		{
 			// このスクリプトがアタッチされているオブジェクトについている
@@ -17,5 +19,60 @@ namespace BokenshaChan
 			// ex.) Player : IMovable なら Playerコンポーネントを取得
 			_movable = GetComponent<IMovable>();
 		}
+		public void Enable()
+		{
+			if (_moveAction == null) return;
+
+			_moveAction.action.started += OnMoveStarted;
+			_moveAction.action.performed += OnMovePerformed;
+			_moveAction.action.canceled += OnMoveCanceled;
+			_moveAction.action.Enable();
+		}
+		public void Disable()
+		{
+			if (_moveAction == null) return;
+
+			_moveAction.action.started -= OnMoveStarted;
+			_moveAction.action.performed -= OnMovePerformed;
+			_moveAction.action.canceled -= OnMoveCanceled;
+			_moveAction.action.Disable();
+		}
+		private void OnEnable() => Enable();
+		private void OnDisable() => Disable();
+
+		private void OnMoveStarted(InputAction.CallbackContext context)
+		{
+			_isMoving = true;
+			_currentMoveInput = context.ReadValue<Vector2>();
+			StartCoroutine(ContinuousMovementCoroutine());
+		}
+
+		private void OnMovePerformed(InputAction.CallbackContext context)
+		{
+			_currentMoveInput = context.ReadValue<Vector2>();
+		}
+
+		private void OnMoveCanceled(InputAction.CallbackContext context)
+		{
+			_isMoving = false;
+			_currentMoveInput = Vector2.zero;
+		}
+		private IEnumerator ContinuousMovementCoroutine()
+		{
+			while (_isMoving)
+			{
+				int x = Mathf.RoundToInt(_currentMoveInput.x);
+				int y = Mathf.RoundToInt(_currentMoveInput.y);
+
+				if ((x != 0 && y == 0) || (x == 0 && y != 0))
+				{
+					_movable.Move(x, y);
+				}
+
+				yield return new WaitForSeconds(_moveCheckInterval);
+			}
+		}
+
+
 	}
 }
--- Assets/Scripts/InputHandler/PlayerInputHandler.cs
+++ Assets/Scripts/InputHandler/PlayerInputHandler.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,78 +6,17 @@ namespace BokenshaChan
 {
 	public class PlayerInputHandler : MonoBehaviour
 	{
-		public IMovable _player;
-		[SerializeField] private InputActionReference _holdMove;
-
-		private Vector2 _currentMoveInput;
-		[SerializeField] private bool _isMoving;
-		[SerializeField] private float _moveCheckInterval = 0.1f; // 移動チェックの間隔
+		private List<IInputHandler> _inputHandlers;
 		void Awake()
 		{
-			_player = GetComponent<Player>();
-
-			if (_holdMove == null)
-			{
-				Debug.Log("ホールドアクションが登録できてないよ");
-				return;
-			}
-			// 入力開始時のイベント
-			_holdMove.action.started += OnMoveStarted;
-			// 入力中のイベント
-			_holdMove.action.performed += OnMovePerformed;
-			// 入力終了時のイベント
-			_holdMove.action.canceled += OnMoveCanceled;
-
-			_holdMove.action.Enable();
-		}
-		private void OnMoveStarted(InputAction.CallbackContext context)
-		{
-			_isMoving = true;
-			_currentMoveInput = context.ReadValue<Vector2>();
-			StartCoroutine(ContinuousMovementCoroutine());
 		}
 
-		private void OnMovePerformed(InputAction.CallbackContext context)
-		{
-			_currentMoveInput = context.ReadValue<Vector2>();
-		}
-
-		private void OnMoveCanceled(InputAction.CallbackContext context)
-		{
-			_isMoving = false;
-			_currentMoveInput = Vector2.zero;
-		}
-
-		private System.Collections.IEnumerator ContinuousMovementCoroutine()
-		{
-			while (_isMoving)
-			{
-				int x = Mathf.RoundToInt(_currentMoveInput.x);
-				int y = Mathf.RoundToInt(_currentMoveInput.y);
-
-				if ((x != 0 && y == 0) || (x == 0 && y != 0))
-				{
-					_player.Move(x, y);
-				}
-
-				yield return new WaitForSeconds(_moveCheckInterval);
-			}
-		}
 
 		private void OnDisable()
 		{
-			if (_holdMove != null && _holdMove.action != null)
-			{
-				_holdMove.action.started -= OnMoveStarted;
-				_holdMove.action.performed -= OnMovePerformed;
-				_holdMove.action.canceled -= OnMoveCanceled;
-			}
+
 		}
 
-		// public void OnInteract(InputAction.CallbackContext context){
-		// 	if(!context.performed) return;
 
-		// 	player.Interact();
-		// }
 	}
 }
