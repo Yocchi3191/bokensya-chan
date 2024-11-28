f8d22501e0e66f1bb1cd28f3fe2a6d77702d6f26
Yocchi3191 <yoshioyocchi314@gmail.com>
2024-11-28 08:07:00 +0000

    2024年11月28日 17:07

--- Assets/Scripts/InputHandler/MoveInputHandler.cs
+++ Assets/Scripts/InputHandler/MoveInputHandler.cs
@@ -1,10 +1,18 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace BokenshaChan
 {
-
 	public class MoveInputHandler : MonoBehaviour, IInputHandler
 	{
-
+		[SerializeField] private InputActionReference _moveAction;
+		private IMovable _movable;
+		private Vector2 _currentMoveInput;
+		private bool _isMoving;
+		[SerializeField] private float _moveCheckInterval = 0.1f;
+		private void Awake()
+		{
+			_movable = GetComponent<IMovable>();
+		}
 	}
 }
