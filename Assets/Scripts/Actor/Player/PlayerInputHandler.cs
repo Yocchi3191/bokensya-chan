676e170eaa9f25cfb5f32bfc490363acb0f52bec
Yocchi3191 <yoshioyocchi314@gmail.com>
2024-11-28 07:42:42 +0000

    2024年11月28日 16:42

--- Assets/Scripts/Actor/Player/PlayerInputHandler.cs
+++ Assets/Scripts/Actor/Player/PlayerInputHandler.cs
@@ -5,23 +5,32 @@ namespace BokenshaChan
 {
 	public class PlayerInputHandler : MonoBehaviour
 	{
-		public IMovable player;
+		public IMovable _player;
+		[SerializeField] private InputActionReference _hold;
 
 		void Awake()
 		{
-			player = GetComponent<Player>();
+			_player = GetComponent<Player>();
+
+			if (_hold == null)
+			{
+				Debug.Log("ホールドアクションが登録できてないよ");
+				return;
+			}
+			_hold.action.performed += OnMove;
+			_hold.action.Enable();
 		}
 		public void OnMove(InputAction.CallbackContext context)
 		{
 			// Player null なら早期リターン
-			if (player == null) return;
+			if (_player == null) return;
 
 			Vector2 input = context.ReadValue<Vector2>();
 			int x = (int)input.x;
 			int y = (int)input.y;
 
 			// 入力があれば移動関数を実行
-			if (x != 0 || y != 0) player.Move(x, y);
+			if (x != 0 || y != 0) _player.Move(x, y);
 			Debug.Log($"ボタン状態: {context.phase}");
 		}
 
